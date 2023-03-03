185ba8b0d7cc6c20120eb92680fdaa4ca50d8de3
Stephen J Carnam <steveorevo@gmail.com>
2023-03-03 11:41:01 -0800

    wip

--- ghost.php
+++ ghost.php
@@ -18,6 +18,7 @@ if ( ! class_exists( 'Ghost') ) {
             global $hcpp;
             $hcpp->ghost = $this;
             $hcpp->add_action( 'invoke_plugin', [ $this, 'setup' ] );
+            $hcpp->add_action( 'render_page', [ $this, 'render_page' ] );
         }
 
         // Setup Ghost with the given user options
@@ -48,9 +49,111 @@ if ( ! class_exists( 'Ghost') ) {
             shell_exec( $cmd );
 
             // Copy over nodebb config files
-            $hcpp->copy_folder( __DIR__ . '/nodeapp', $nodebb_folder, $user );
+            $hcpp->copy_folder( __DIR__ . '/nodeapp', $ghost_folder, $user );
+
+            // Setup ghost if creds given
+            // http://test3.openmy.info/ghost/#/setup
+            // Start up ghost
+            // export NODE_ENV=production;node current/index.js
         }
 
+        // Customize the install page
+        public function render_page( $args ) {
+            global $hcpp;
+            if ( strpos( $_SERVER['REQUEST_URI'], '/add/webapp/?app=Ghost&' ) === false ) return $args;
+            $content = $args['content'];
+            $user = trim($args['user'], "'");
+            $shell = $hcpp->run( "list-user $user json")[$user]['SHELL'];
+
+            // Suppress Data loss alert, and PHP version selector
+            $content = '<style>.form-group:last-of-type,.alert.alert-info.alert-with-icon{display:none;}</style>' . $content;
+            if ( $shell != 'bash' ) {
+
+                // Display bash requirement
+                $content = '<style>.form-group{display:none;}</style>' . $content;
+                $msg = '<div style="margin-top:-20px;width:75%;"><span>';
+                $msg .= 'Cannot contiue. User "' . $user . '" must have bash login ability.</span>';
+                $msg .= '<script>$(function(){$(".l-unit-toolbar__buttonstrip.float-right a").css("display", "none");});</script>';
+            }elseif ( !is_dir('/usr/local/hestia/plugins/nodeapp') ) {
+        
+                // Display missing nodeapp requirement
+                $content = '<style>.form-group{display:none;}</style>' . $content;
+                $msg = '<div style="margin-top:-20px;width:75%;"><span>';
+                $msg .= 'Cannot contiue. The NodeBB Quick Installer requires the NodeApp plugin.</span>';
+                $msg .= '<script>$(function(){$(".l-unit-toolbar__buttonstrip.float-right a").css("display", "none");});</script>';
+            }else{
+        
+                // Display install information
+                $msg = '<div style="margin-top:-20px;width:75%;"><span>';
+                $msg .= 'The Ghost instance lives inside the "nodeapp" folder (adjacent to "public_html"). ';
+                $msg .= 'It can be a standalone instance in the domain root, or in a subfolder using the ';
+                $msg .= '<b>Install Directory</b> field below.</span><br><span style="font-style:italic;color:darkorange;">';
+                $msg .= 'Files will be overwritten; be sure the specified <span style="font-weight:bold">Install Directory</span> is empty!</span></div><br>';
+                
+                // Enforce username and password, remove PHP version
+                $msg .= '
+                <script>
+                    $(function() {
+                        $("label[for=webapp_php_version]").parent().css("display", "none");
+                        let borderColor = $("#webapp_ghost_username").css("border-color");
+                        let toolbar = $(".l-center.edit").html();
+                        function nr_validate() {
+                            if ( $("#webapp_ghost_username").val().trim() == "" || $("#webapp_ghost_password").val().trim() == "" || $("#webapp_ghost_email").val().trim() == "" ) {
+                                $(".l-unit-toolbar__buttonstrip.float-right a").css("opacity", "0.5").css("cursor", "not-allowed");
+                                if ($("#webapp_ghost_username").val().trim() == "") {
+                                    $("#webapp_ghost_username").css("border-color", "red");
+                                }else{
+                                    $("#webapp_ghost_username").css("border-color", borderColor);
+                                }
+                                if ($("#webapp_ghost_password").val().trim() == "") {
+                                    $("#webapp_ghost_password").css("border-color", "red");
+                                }else{
+                                    $("#webapp_ghost_password").css("border-color", borderColor);
+                                }
+                                if ($("#webapp_ghost_email").val().trim() == "") {
+                                    $("#webapp_ghost_email").css("border-color", "red");
+                                }else{
+                                    $("#webapp_ghost_email").css("border-color", borderColor);
+                                }
+                                return false;
+                            }else{
+                                $(".l-unit-toolbar__buttonstrip.float-right a").css("opacity", "1").css("cursor", "");
+                                $("#webapp_ghost_username").css("border-color", borderColor);
+                                $("#webapp_ghost_password").css("border-color", borderColor);
+                                $("#webapp_ghost_email").css("border-color", borderColor);
+                                return true;
+                            }
+                        };
+        
+                        // Override the form submition
+                        $(".l-unit-toolbar__buttonstrip.float-right a").removeAttr("data-action").removeAttr("data-id").click(function() {
+                            if ( nr_validate() ) {
+                                $(".l-sort.clearfix").html("<div class=\"l-unit-toolbar__buttonstrip\"></div><div class=\"l-unit-toolbar__buttonstrip float-right\"><div><div class=\"timer-container\" style=\"float:right;\"><div class=\"timer-button spinner\"><div class=\"spinner-inner\"></div><div class=\"spinner-mask\"></div> <div class=\"spinner-mask-two\"></div></div></div></div></div>");
+                                $("#vstobjects").submit();
+                            }
+                        });
+                        $("#vstobjects").submit(function(e) {
+                            if ( !nr_validate() ) {
+                                e.preventDefault();
+                            }
+                        });
+                        $("#webapp_ghost_username").blur(nr_validate).keyup(nr_validate);
+                        $("#webapp_ghost_password").blur(nr_validate).keyup(nr_validate);
+                        $("#webapp_ghost_email").blur(nr_validate).keyup(nr_validate);
+                        $(".generate").click(function() {
+                            setTimeout(function() {
+                                nr_validate();
+                            }, 500)
+                        });
+                        nr_validate();
+                    });
+                </script>
+                ';
+            }
+            $content = str_replace( '<div class="app-form">', '<div class="app-form">' . $msg, $content );
+            $args['content'] = $content;
+            return $args;
+        }
     }
     new Ghost();
 }
