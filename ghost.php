39eb59cabaf82128a43bb6a6419d897bea37be23
Stephen J Carnam <steveorevo@gmail.com>
2023-03-03 10:54:21 -0800

    wip

--- ghost.php
+++ ghost.php
@@ -22,11 +22,33 @@ if ( ! class_exists( 'Ghost') ) {
 
         // Setup Ghost with the given user options
         public function setup( $args ) {
-            if ( $args[0] != 'nodebb_install' ) return $args;
+            if ( $args[0] != 'ghost_install' ) return $args;
             global $hcpp;
             $options = json_decode( $args[1], true );
             $user = $options['user'];
             $domain = $options['domain'];
+
+            // Copy the Ghost files to the user folder
+            $ghost_folder = $options['ghost_folder'];
+            if ( $ghost_folder == '' || $ghost_folder[0] != '/' ) $ghost_folder = '/' . $ghost_folder;
+            $nodeapp_folder = "/home/$user/web/$domain/nodeapp";
+            $subfolder = $ghost_folder;
+            $ghost_folder = $nodeapp_folder . $ghost_folder;
+
+            // Create the nodeapp folder 
+            $cmd = "mkdir -p " . escapeshellarg( $ghost_folder ) . " && ";
+            $cmd .= "chown -R $user:$user " . escapeshellarg( $nodeapp_folder );
+            shell_exec( $cmd );
+
+            // Copy over ghost core files
+            $cmd = 'cd ' . escapeshellarg( $ghost_folder ) . ' && ';
+            $cmd .= 'runuser -l ' . $user . ' -c "cp /opt/ghost/.ghost-cli ./" && ';
+            $cmd .= 'runuser -l ' . $user . ' -c "cp -r /opt/ghost/content ./" && ';
+            $cmd .= 'runuser -l ' . $user . ' -c "cp -r /opt/ghost/current ./"';
+            shell_exec( $cmd );
+
+            // Copy over nodebb config files
+            $hcpp->copy_folder( __DIR__ . '/nodeapp', $nodebb_folder, $user );
         }
 
     }
