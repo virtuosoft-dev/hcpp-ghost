aad32667387049f7614263ab5e1b67896cbf0814
Stephen J Carnam <steveorevo@gmail.com>
2023-03-03 12:13:08 -0800

    wip

--- Installers/Ghost/GhostSetup.php
+++ Installers/Ghost/GhostSetup.php
@@ -9,7 +9,11 @@ class GhostSetup extends BaseSetup {
 		"name" => "Ghost",
 		"group" => "cms",
 		"enabled" => true,
-		"version" => "latest",
+		"version" => (function() {
+			$v = shell_exec( 'ls -la /opt/ghost/current' );
+			$v = trim( array_pop( explode( '/', $v ) ) );
+			return $v;
+		})(),
 		"thumbnail" => "ghost-thumb.png",
 	];
 
