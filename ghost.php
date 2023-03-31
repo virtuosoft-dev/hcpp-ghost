b4cd9dc569690de3beb501e0e29bf11366b2169c
Steveorevo <steve@steveorevo.com>
2023-03-31 16:34:52 -0700

    wip

--- ghost.php
+++ ghost.php
@@ -231,7 +231,7 @@ if ( ! class_exists( 'Ghost') ) {
             if ( strpos( '<div class="app-form">', $content ) !== false ) {
                 $content = str_replace( '<div class="app-form">', '<div class="app-form">' . $msg, $content ); // Hestia 1.6.X
             }else{
-                $content = str_replace( '<h1 ', $msg . '<h1 ', $content ); // Hestia 1.7.X
+                $content = str_replace( '<h1 ', $msg . '<h1 style="padding-bottom:0;" ', $content ); // Hestia 1.7.X
             }
             $args['content'] = $content;
             return $args;
